503069944920fb9545e80ed8c247143550f566a1
Raell Dottin <raell.dottin@gmail.com>
2023-11-26 01:14:41 -0500

    Convert  function and  to async for improved task handling.

--- basic.py
+++ basic.py
@@ -1,23 +1,24 @@
 import time
+import asyncio
 
 
-def do_work(s: str, delay_s: float = 1.0):
+async def do_work(s: str, delay_s: float = 1.0):
     print(f"{s} started")
-    time.sleep(delay_s)
+    await asyncio.sleep(delay_s)
     print(f"{s} done")
 
 
-def main():
+async def main():
     start = time.perf_counter()
 
     todo = ["get package", "laundry", "bake cake"]
 
     for item in todo:
-        do_work(item)
+        await do_work(item)
 
     end = time.perf_counter()
     print(f"it took: {end - start: .2f}s")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
